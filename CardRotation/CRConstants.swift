b25d1afa16a03dad6be614f8d2d21c06b932aa25
Anish Kumar <anaish.92@gmail.com>
2019-12-05 18:52:10 +0530

    Added code for card rotation with clean swift architecture

---
+++ CardRotation/CRConstants.swift
@@ -0,0 +1,15 @@
+//
+//  CRConstants.swift
+//  CardRotation
+//
+//  Created by Anish Kumar on 05/12/19.
+//  Copyright © 2019 Anish Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct CRConstants {
+    static let cardRotationAngle: Float = 20
+    static let radianFactor = Float.pi / 180.0
+    static let animationDuration: CFTimeInterval = 0.5
+}
